05bf6831aa4a7f8eb6cb1f77f64002a884bf088c
brainsandwich <alexandre.beaudet@protonmail.ch>
2017-06-27 15:44:08 +0200

    Deps fetcher ultimate

--- deps_fetcher.py
+++ deps_fetcher.py
@@ -1,24 +1,34 @@
+# This script downloads git repositories and sets them to the
+# specified version / branch you want. It's up to you to use 
+# the downloaded packages correctly.
+# 
+# A deps.json file must be provided (I'll add command args parsing later)
+# containing declaration for dependencies. It also points out where to
+# install the packages
+#
+# Made by brainsandwich
+# Free of use
+
 import json, os, sys, subprocess
 
 def log(msg):
 	print msg
 	sys.stdout.flush()
 
-config_path = 'deps.json'
-
 # init
+config_path = os.path.abspath(sys.argv[1] if len(sys.argv) > 1 else 'deps.json')
 log('> Deps fetcher 1.0')
-
+log('> Configuration file : ' + config_path)
 if not os.path.exists(config_path):
 	sys.exit('! No config found ! Aborting ...')
 
 # parse config
-config_file = open('deps.json', 'r')
+config_file = open(config_path, 'r')
 config = json.loads(config_file.read())
 config_file.close()
 
 # create dir
-deps_path = os.path.abspath(config['path'])
+deps_path = os.path.abspath(config['path'] if 'path' in config else 'external')
 log('> Installing dependencies in ' + deps_path + ' ...')
 if not os.path.exists(deps_path):
     os.makedirs(deps_path)
@@ -28,11 +38,17 @@ for package in config['packages']:
 	name = package['name']
 	log('> Fetching package ' + name)
 
+	# if origin is not a url, it's a filepath and
+	# we need its absolute value
 	origin = package['origin']
+	if not origin[:4] == 'http':
+		origin = os.path.abspath(origin)
+
+	# one can either provide a version or a branch
 	version = package['version'] if 'version' in package else ''
 	branch = package['branch'] if 'branch' in package else ''
 
-	# init repo
+	# init repo in repo_dir
 	repo_dir = os.path.join(deps_path, name)
 	if not os.path.exists(repo_dir):
 		os.makedirs(repo_dir)
@@ -40,7 +56,7 @@ for package in config['packages']:
 		subprocess.call(['git', 'remote', 'add', 'origin', origin], cwd=repo_dir)
 		subprocess.call(['git', 'checkout', '-b', 'target'], cwd=repo_dir)
 
-	# fetch branch / version
+	# fetch branch / version (this will effectively download stuff)
 	subprocess.call(['git', 'fetch'], cwd=repo_dir)
 	if version != '':
 		subprocess.call(['git', 'reset', '--hard', version], cwd=repo_dir)
