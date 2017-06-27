ea29b925d64c0326c9b340be507ee49ce03a4499
brainsandwich <alexandre.beaudet@protonmail.ch>
2017-06-27 15:29:47 +0200

    Deps improved again

--- deps_fetcher.py
+++ deps_fetcher.py
@@ -1,9 +1,14 @@
 import json, os, sys, subprocess
 
+def log(msg):
+	print msg
+	sys.stdout.flush()
+
 config_path = 'deps.json'
 
 # init
-print '> Deps fetcher 1.0'
+log('> Deps fetcher 1.0')
+
 if not os.path.exists(config_path):
 	sys.exit('! No config found ! Aborting ...')
 
@@ -13,16 +18,19 @@ config = json.loads(config_file.read())
 config_file.close()
 
 # create dir
-deps_path = os.path.abspath(config["path"])
-print '> Installing dependencies in ' + deps_path + ' ...'
+deps_path = os.path.abspath(config['path'])
+log('> Installing dependencies in ' + deps_path + ' ...')
 if not os.path.exists(deps_path):
     os.makedirs(deps_path)
 
 # clone packages (assume all git for now)
-for package in config["packages"]:
-	name = package["name"]
-	origin = package["origin"]
-	version = package["version"]
+for package in config['packages']:
+	name = package['name']
+	log('> Fetching package ' + name)
+
+	origin = package['origin']
+	version = package['version'] if 'version' in package else ''
+	branch = package['branch'] if 'branch' in package else ''
 
 	# init repo
 	repo_dir = os.path.join(deps_path, name)
@@ -30,8 +38,11 @@ for package in config["packages"]:
 		os.makedirs(repo_dir)
 		subprocess.call(['git', 'init', repo_dir])
 		subprocess.call(['git', 'remote', 'add', 'origin', origin], cwd=repo_dir)
-		subprocess.call(['git', 'fetch'], cwd=repo_dir)
-		subprocess.call(['git', 'checkout', version], cwd=repo_dir)
+		subprocess.call(['git', 'checkout', '-b', 'target'], cwd=repo_dir)
+
+	# fetch branch / version
+	subprocess.call(['git', 'fetch'], cwd=repo_dir)
+	if version != '':
+		subprocess.call(['git', 'reset', '--hard', version], cwd=repo_dir)
 	else:
-		subprocess.call(['git', 'fetch'], cwd=repo_dir)
-		subprocess.call(['git', 'checkout', version], cwd=repo_dir)
+		subprocess.call(['git', 'reset', '--hard', 'origin/' + branch], cwd=repo_dir)
